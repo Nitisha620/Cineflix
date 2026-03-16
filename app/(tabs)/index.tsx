ef9055cb21e395d952ac8c5236c2cae404a9a390
Nitisha620 <nitishajain94@gmail.com>
2026-03-16 10:18:12 +0530

    Added Logo and image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,54 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl font-bold text-light-200">
-        Welcome to CineFlix!
-      </Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#030014",
+      }}
+    >
+      <Image
+        style={{
+          position: "absolute",
+          width: "100%",
+          zIndex: 0,
+        }}
+        source={images.bg}
+      />
+      <ScrollView
+        style={{
+          flex: 1,
+          padding: 5,
+        }}
+        showsVerticalScrollIndicator = {false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image
+          style={{
+            width: 40,
+            height: 100,
+            marginTop: 60,
+            marginBottom: 0,
+            margin: "auto",
+          }}
+          source={icons.logo}
+        />
+        <Image
+          style={{
+            width: 40,
+            height: 100,
+            marginTop: 60,
+            marginBottom: 0,
+            margin: "auto",
+          }}
+          source={icons.logo}
+        />
+      </ScrollView>
     </View>
   );
 }
